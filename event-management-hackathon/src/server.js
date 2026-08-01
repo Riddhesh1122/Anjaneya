11c09872be57d9d32b3b038f80b0458ceddf609c
Vaibhav kiran sapkale <vsapkale22@gmail.com>
2026-08-01 11:52:09 +0530

    feat(frontend): add basic static homepage and update server to serve static files

--- event-management-hackathon/src/server.js
+++ event-management-hackathon/src/server.js
@@ -1,14 +1,10 @@
 const express = require('express');
+const path = require('path');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const morgan = require('morgan');
-const connectDB = require('./config/db');
 
-// Load environment variables
 dotenv.config();
-
-// Connect to MongoDB
-connectDB();
 
 const app = express();
 
@@ -17,16 +13,17 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 
-// Simple test route
+// Serve static files (HTML, CSS, JS)
+app.use(express.static(path.join(__dirname, '../public')));
+
+// Home route
 app.get('/', (req, res) => {
-	res.json({ 
-		message: 'Event Management API is running successfully!' 
-	});
+  res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 // Start server
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-	console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
